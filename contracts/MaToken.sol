018b6faf0f7ec52a0e2bbf68499f1f316ae3e5a1
Daylen-Mackey <dmackey@ualberta.ca>
2021-03-30 21:03:21 -0600

    Add functionality & tests for token transfers

--- contracts/MaToken.sol
+++ contracts/MaToken.sol
@@ -13,20 +13,26 @@ contract MaToken{
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     constructor(uint256 _initialSupply) public{
         //msg is a global var in solidity --> sender is the address of the account that calls this function
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
-
         //Allocate the initial supply 
-
-
     }
 // Transfer function
     function transfer(address _to, uint256 _value) public returns (bool success) {
         // Exception if you don't have enough 
         // Returns boolean 
         require(balanceOf[msg.sender] >= _value); //If True, continue running, if false, throw error 
-
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/MaToken.js
+++ test/MaToken.js
@@ -47,16 +47,26 @@ contract("MaToken", function (accounts) {
 
         return MaToken.deployed().then(function (instance){
             tokenInstance = instance
-
-
             return tokenInstance.transfer.call(accounts[1],10000000)
         }).then(assert.fail).catch(function(err){
             assert(err.message.toString().includes("revert"), "Error message must contain revert")
+            return tokenInstance.transfer.call(accounts[1], 250000,{from : accounts[0]})
+        }).then(function(success){
+
+            assert.equal(success,true, "Successful transfer returns true")
+            return tokenInstance.transfer(accounts[1], 250000,{from : accounts[0]})
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, "Triggers one event")
+            assert.equal(receipt.logs[0].event , "Transfer", "Is a transfer event")
+            assert.equal(receipt.logs[0].args._from, accounts[0], "Logs where the token is from")
+            assert.equal(receipt.logs[0].args._to, accounts[1], "Logs where the token is going")
+            assert.equal(receipt.logs[0].args._value, 250000, "Logs the proper transfer amount")
+            return tokenInstance.balanceOf(accounts[1])
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 250000, "Ensure the transfer went through correctly")
+            return tokenInstance.balanceOf(accounts[0])
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 750000, "deducts the amount")
         })
-
-
-
     })
-
-
 })
